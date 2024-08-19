3693870e7bd4b4e194160de5111c07078ddbe8a5
David Luong <camkhang.bk.hust@gmail.com>
2024-08-19 16:03:00 +0700

    add new file

--- selenium/javaTester/Topic_01_Data_Type.java
+++ selenium/javaTester/Topic_01_Data_Type.java
@@ -1,11 +1,78 @@
 package javaTester;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
 public class Topic_01_Data_Type {
     // Kiểu dữ liệu trongn Java - 2 nhóm
 
     // I - Kiểu dữ liệu nguyên thuỷ (Primitive Type)
-    // 8 Loại
+    // Số nguyên: Buyte - Short - int - Long
+    // Không có phần thập phân: nhân viên 1 cty/ học csih 1 lớp/ trường/...
+    byte bNumber = 40;
+    short sNumber = 3000;
+    int iNumber = 12345678;
+    long lNumgber = 1234567891;
 
+    //Số thực: float - double
+    // Có phần thập phân: Diểm số/ luương/...
+    float fNumber = 9.99f;
+    double dNumber = 9.99d;
+
+
+    // Ký tự: Char
+    // Đại diện cho 1 ký tự
+    char c = 'm';
+    char d ='i';
+
+    // Logic: boolean
+    // Kết quả bài test: Pass/Fail (ko ngoại lệ)
+    boolean status = true;
+    boolean status1 = false;
 
     //II - Kiểu dữ liệu tham chiếu (Reference Type)
+    // Class
+    FirefoxDriver firefoxDriver =new FirefoxDriver();
+    Select select = new Select(firefoxDriver.findElement(By.className("")));
+
+    Topic_01_Data_Type topic01 = new Topic_01_Data_Type();
+
+    // Interface
+    WebDriver driver;
+    JavascriptExecutor jsExecutor;
+
+    // object
+    Object name = "Automation FC";
+
+    // Array (Kiểu nguyên thuỷ/ Tham chiếu)
+    int[] studentAge = {15, 20, 8};
+    String [] studentName = {"Automation", "Testing"};
+
+    // Collection: Lisst/ Set/ Queue
+    List<String> studentAddress = new ArrayList<>();
+    List<String> studentCity = new LinkedList<>();
+    List<String> studentPhone = new Vector<>();
+
+
+    // String - Chuỗi ký tự
+
+    String fullName = "Hồ Chí Minh";
+
+
+    // Khia báo  bến đẻ lư trữ 1 loại dữ liệu nào đó
+    // Access Modifier: Phạm vi truy cập (public/ private/ protected/ default)
+    //Kiểu dữ liệu
+    // Tên biến
+    // Giá trị của biến - gán vào với phép =
+    // Nếu như k gán: dữ liệu mặc đinh
+    public int studentNumber = 200;
 }
--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -29,38 +30,88 @@ public class Topic_02_Selenium_Locator {
 
     @Test
     public void TC_01_ID(){
-        //driver.findElement(By.);
-
+        // Timf element cos id laf firstName
+        driver.findElement(By.id("FirstName")).sendKeys("Keane");
     }
     @Test
     public void TC_02_Class(){
-
+        driver.findElement(By.className("header-logo"));
     }
 
     @Test
     public void TC_03_Name(){
-
+        driver.findElement(By.name("DateOfBirthday"));
     }
 
     @Test
     public void TC_04_Tagname(){
-
+        driver.findElement(By.tagName("input"));
     }
 
     @Test
     public void TC_05_LinkText(){
-
+        // Độ chính xác cao = tuyêt đối = Toàn bộ
+        driver.findElement(By.linkText("Shiping & returns"));
     }
 
     @Test
     public void TC_06_Partial_LinK(){
-
+        // Độ chính xác không cao = tương đối = 1 Phần(đầu/ giữa/ cuối)
+        driver.findElement(By.partialLinkText("for  vendor"));
+        driver.findElement(By.partialLinkText("vendor account"));
+        driver.findElement(By.partialLinkText("Apply for  vendor"));
     }
 
     @Test
-    public void TC_07_XPath(){
+    public void TC_07_Css(){
+        // Css vs id
+        driver.findElement(By.cssSelector("input[id='FirstName']"));
+        driver.findElement(By.cssSelector("input#FirstName"));
+        driver.findElement(By.cssSelector("FirstName"));
+
+        // Css vs Class
+        driver.findElement(By.cssSelector("div[class='FirstName']"));
+        driver.findElement(By.cssSelector("div.page-title"));
+        driver.findElement(By.cssSelector(".page-title"));
+
+        // Css vs Name
+        driver.findElement(By.cssSelector("input[name='FirstName']"));
+
+        // Css vs Tagname
+        driver.findElement(By.cssSelector("input"));
+
+        // Css vs Link
+        driver.findElement(By.cssSelector("a[href='customer/addresses']"));
+
+        // Css vs partial link
+        driver.findElement(By.cssSelector("a[href*='addresses']"));
+        driver.findElement(By.cssSelector("a[href^='addresses']"));
+        driver.findElement(By.cssSelector("a[href$='addresses']"));
+    }
+
+    public void TC_08_XPath(){
+        // XPath vs id
+        driver.findElement(By.xpath("//input[@id='FirstName']"));
+
+        // XPath vs Class
+        driver.findElement(By.xpath("//div[@class='FirstName']"));
+
+        // XPath vs Name
+        driver.findElement(By.xpath("input[name='FirstName']"));
+
+        // XPath vs Tagname
+        driver.findElement(By.xpath("//input"));
+
+        // XPath vs Link
+        driver.findElement(By.xpath("//a[@href='customer/addresses']"));
+        driver.findElement(By.xpath("//a[text()='addresses']"));
+
+        // XPath vs partial link
+        driver.findElement(By.xpath("//a[contains(href*='addresses'])"));
+        driver.findElement(By.xpath("//a[contains(text()='addresses')]"));
 
     }
+
     @AfterClass
     public void afterClass(){
         driver.quit();

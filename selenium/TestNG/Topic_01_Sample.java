47e3e2b88e9740202c3580200f31f1433bb519d3
David Luong <camkhang.bk.hust@gmail.com>
2024-11-30 20:52:05 +0700

    TestNG topic 02

---
+++ selenium/TestNG/Topic_01_Sample.java
@@ -0,0 +1,10 @@
+package TestNG;
+
+import java.util.Random;
+
+public class Topic_01_Sample {
+
+    private int getRandomNumber(){
+        return new Random().nextInt(100000);
+    }
+}
---
+++ selenium/TestNG/Topic_02_Annotations.java
@@ -0,0 +1,71 @@
+package TestNG;
+
+import org.testng.annotations.*;
+
+public class Topic_02_Annotations {
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After Class");
+    }
+
+    @BeforeGroups
+    public void beforeGroups(){
+        System.out.println("Before Ground");
+    }
+
+    @AfterGroups
+    public void afterGroups(){
+        System.out.println("After Groups");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("before Method");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("After Method");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("before Suite");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("After Suite");
+    }
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("before Test");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("After Test");
+    }
+
+    @Test
+    public void test01(){
+        System.out.println("Test 01");
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("Test 02");
+    }
+    @Test
+    public void test03(){
+        System.out.println("Test 03");
+    }
+
+}
